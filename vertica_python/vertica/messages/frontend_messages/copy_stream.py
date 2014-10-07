205b5033a4feceb77c2a30678ef393f9ee0f9e7d
Johan Olofsson <johano99@hotmail.com>
2014-10-07 11:29:23 +0200

    added stream support to copy command

---
+++ vertica_python/vertica/messages/frontend_messages/copy_stream.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+from struct import pack
+
+from vertica_python.vertica.messages.message import FrontendMessage
+
+class CopyStream(FrontendMessage):
+
+    def __init__(self, stream, buffer_size=131072):
+        self.stream = stream
+        self.bufsize = buffer_size
+
+    def read_bytes(self):
+
+        data = self.stream.read(self.bufsize)
+
+        if len(data) == 0:
+            return data
+
+        return self.message_string(data)
+
+CopyStream._message_id('d')

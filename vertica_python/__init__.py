439e366d277328630baa84c06424be57e0823005
Alex Kim <alex.kim@uber.com>
2015-04-13 11:34:20 -0700

    bump to v 0.3.6

--- vertica_python/__init__.py
+++ vertica_python/__init__.py
@@ -6,7 +6,7 @@ from vertica_python.vertica.connection import Connection
 # Main module for this library.
 
 # The version number of this library.
-version_info = (0, 3, 5)
+version_info = (0, 3, 6)
 __version__ = '.'.join(map(str, version_info))
 
 __author__ = 'Uber Technologies, Inc'

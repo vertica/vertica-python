f7322ec98f6e9da6677621470aae190b229e0770
Timothy Heys <theys@kayak.com>
2016-11-28 11:26:02 +0100

    Fixes #133  Evaluation of boolean values in python3

--- vertica_python/tests/type_tests.py
+++ vertica_python/tests/type_tests.py
@@ -1,15 +1,31 @@
+from decimal import Decimal
+
 from .test_commons import conn_info, VerticaTestCase
 from .. import connect
-from decimal import Decimal
 
 
 class TypeTestCase(VerticaTestCase):
-    def test_decimal_query(self):
-        value = Decimal(0.42)
-        query = "SELECT {}::numeric".format(value)
-
+    @staticmethod
+    def _execute_and_fetchone(query):
         with connect(**conn_info) as conn:
             cur = conn.cursor()
             cur.execute(query)
-            res = cur.fetchone()
-            self.assertAlmostEqual(res[0], value)
+            return cur.fetchone()
+
+    def test_decimal_query(self):
+        value = Decimal(0.42)
+        query = "SELECT {}::numeric".format(value)
+        res = self._execute_and_fetchone(query)
+        self.assertAlmostEqual(res[0], value)
+
+    def test_boolean_query__true(self):
+        value = True
+        query = "SELECT {}::boolean".format(value)
+        res = self._execute_and_fetchone(query)
+        self.assertEqual(res[0], value)
+
+    def test_boolean_query__false(self):
+        value = False
+        query = "SELECT {}::boolean".format(value)
+        res = self._execute_and_fetchone(query)
+        self.assertEqual(res[0], value)

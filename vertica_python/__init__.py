6e38ae103434b3fe649163fec44c90863d956486
Alex Kim <alex.kim@uber.com>
2016-11-28 10:55:58 -0800

    0.6.9

--- vertica_python/__init__.py
+++ vertica_python/__init__.py
@@ -6,7 +6,7 @@ from vertica_python.vertica.connection import Connection
 # Main module for this library.
 
 # The version number of this library.
-version_info = (0, 6, 8)
+version_info = (0, 6, 9)
 
 __version__ = '.'.join(map(str, version_info))
 

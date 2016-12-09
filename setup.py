ee2a095449459251f01840a70e43c5572e54bb99
Alex Kim <alex.kim@uber.com>
2016-12-09 11:16:11 -0800

    0.6.10

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ opts = ReqOpts(None, 'git')
 # version should use the format 'x.x.x' (instead of 'vx.x.x')
 setup(
     name='vertica-python',
-    version='0.6.9',
+    version='0.6.10',
     description='A native Python client for the Vertica database.',
     author='Justin Berka, Alex Kim',
     author_email='justin.berka@gmail.com, alex.kim@uber.com',

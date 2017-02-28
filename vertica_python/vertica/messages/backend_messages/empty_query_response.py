59057c28746220cd0c9d9c78d4fe18b6480e8dda
Ian Zelikman <Ian.Zelikman@ibm.com>
2017-02-27 19:36:28 -0500

    Add init for empty query response

--- vertica_python/vertica/messages/backend_messages/empty_query_response.py
+++ vertica_python/vertica/messages/backend_messages/empty_query_response.py
@@ -4,7 +4,8 @@ from vertica_python.vertica.messages.message import BackendMessage
 
 
 class EmptyQueryResponse(BackendMessage):
-    pass
+    def __init__(self, data=None):
+        self.data = data
 
 
 EmptyQueryResponse._message_id(b'I')

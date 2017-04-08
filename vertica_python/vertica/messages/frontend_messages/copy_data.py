27f15b40555515d8d9dd375f5a7b0d6577d69f88
Ben Feinstein <feinsteinben@gmail.com>
2017-04-08 15:49:36 +0300

    Add unicode_error parameters to streaming and data copies

--- vertica_python/vertica/messages/frontend_messages/copy_data.py
+++ vertica_python/vertica/messages/frontend_messages/copy_data.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division, absolute_import
 
-from struct import pack
+from six import text_type, binary_type
 
 from ..message import BulkFrontendMessage
 
@@ -10,12 +10,17 @@ UTF_8 = 'utf-8'
 class CopyData(BulkFrontendMessage):
     message_id = b'd'
 
-    def __init__(self, data):
+    def __init__(self, data, unicode_error='strict'):
         BulkFrontendMessage.__init__(self)
-        self._data = data
+        self._unicode_error = unicode_error
+        if isinstance(data, text_type):
+            self._data = self._data.encode(encoding=UTF_8, errors=self._unicode_error)
+        elif isinstance(data, binary_type):
+            self._data = data
+        else:
+            raise TypeError("should be string or bytes")
 
     def read_bytes(self):
         # to deal with unicode strings
-        encoded = self._data.encode(UTF_8)
-        bytes_ = pack('{0}s'.format(len(encoded)), encoded)
+        bytes_ = self._data
         return bytes_

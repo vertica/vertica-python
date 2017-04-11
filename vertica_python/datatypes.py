45f1e2346911d2d326cb6404e7de773e8ad28de4
Ben Feinstein <feinsteinben@gmail.com>
2017-04-12 01:06:08 +0300

    Bug and compatibility fixes in in datatypes.py and errors.py
    - Fixed imports from datetime in datatypes.py.
    - Used datetime.utcfromtimestamp(...) instead of adding timedelta to EPOCH.

--- vertica_python/datatypes.py
+++ vertica_python/datatypes.py
@@ -1,33 +1,38 @@
+from __future__ import print_function, division, absolute_import
+
+from datetime import date, datetime, time
 
 
-from datetime import datetime
-from datetime import timedelta
-
-
+# noinspection PyPep8Naming
 def Date(year, month, day):
-    return datetime.date(year, month, day)
+    return date(year, month, day)
 
 
+# noinspection PyPep8Naming
 def Time(hour, minute, second):
-    return datetime.time(hour, minute, second)
+    return time(hour, minute, second)
 
 
+# noinspection PyPep8Naming
 def Timestamp(year, month, day, hour, minute, second):
-    return datetime.datetime(year, month, day, hour, minute, second)
+    return datetime(year, month, day, hour, minute, second)
 
 
+# noinspection PyPep8Naming
 def DateFromTicks(ticks):
-    d = datetime(1970, 1, 1) + timedelta(seconds=ticks)
+    d = datetime.utcfromtimestamp(ticks)
     return d.date()
 
 
+# noinspection PyPep8Naming
 def TimeFromTicks(ticks):
-    d = datetime(1970, 1, 1) + timedelta(seconds=ticks)
+    d = datetime.utcfromtimestamp(ticks)
     return d.time()
 
 
+# noinspection PyPep8Naming
 def TimestampFromTicks(ticks):
-    d = datetime(1970, 1, 1) + timedelta(seconds=ticks)
+    d = datetime.utcfromtimestamp(ticks)
     return d.time()
 
 
@@ -35,8 +40,10 @@ class Bytea(str):
     pass
 
 
+# noinspection PyPep8Naming
 def Binary(string):
     return Bytea(string)
+
 
 # vertica doesnt have a binary or row_id type i think
 STRING = 9

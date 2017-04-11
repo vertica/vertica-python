e51e6fb993bca9a9d53d99729435df0a6de92166
Ben Feinstein <feinsteinben@gmail.com>
2017-04-12 01:11:58 +0300

    Adding executemany to cursor.py
    - Adding executemany.
    - Improving Cursor.copy(...) by always using stream messages (even from text).
    - Bug fixes in fetchone() - previuosly, it didn't support multi-statements so well. After nextset(), it didn't consume all the ParameterStatus messages (and such) before returning the message. Now it does so.

--- vertica_python/vertica/cursor.py
+++ vertica_python/vertica/cursor.py
@@ -1,45 +1,74 @@
+from __future__ import print_function, division, absolute_import
 
-
-import re
 import logging
-
+import re
 from collections import OrderedDict
+from io import IOBase
+
+import six
+# noinspection PyUnresolvedReferences,PyCompatibility
 from builtins import str
+from six import binary_type, text_type, string_types, BytesIO, StringIO
 
-import vertica_python.errors as errors
+try:
+    from psycopg2.extensions import QuotedString
+except ImportError:
+    class QuotedString(object):
+        def __init__(self, s):
+            raise ImportError("couldn't import psycopg2.extensions.QuotedString")
 
-import vertica_python.vertica.messages as messages
-from vertica_python.vertica.column import Column
+from .. import errors
+from ..compat import as_text
+from ..vertica import messages
+from ..vertica.column import Column
 
 logger = logging.getLogger('vertica')
 
+UTF_8 = 'utf-8'
+
+if six.PY2:
+    # noinspection PyUnresolvedReferences
+    file_type = (IOBase, file)
+elif six.PY3:
+    file_type = (IOBase,)
+
+
 class Cursor(object):
-    def __init__(self, connection, cursor_type=None, unicode_error='strict'):
+    _insert_statement = re.compile(
+        u"INSERT\\s+INTO"
+        "\\s+((?P<schema>{id})\\.)?(?P<table>{id})"
+        "\\s*\\(\\s*(?P<variables>({id}(\\s*,\\s*{id})*)?\\s*)\\)"
+        "\\s+VALUES\\s*\\(\\s*(?P<values>.*)\\)".format(id=u"[a-zA-Z_][\\w\\d\\$_]*"), re.U | re.I)
+
+    def __init__(self, connection, cursor_type=None, unicode_error=None):
         self.connection = connection
         self.cursor_type = cursor_type
-        self.unicode_error = unicode_error
+        self.unicode_error = unicode_error if unicode_error is not None else 'strict'
         self._closed = False
         self._message = None
 
         self.error = None
 
         #
-        # dbApi properties
+        # dbapi properties
         #
         self.description = None
         self.rowcount = -1
         self.arraysize = 1
 
+    #############################################
+    # supporting `with` statements
+    #############################################
     def __enter__(self):
         return self
 
     def __exit__(self, type_, value, traceback):
         self.close()
 
-    #
-    # dbApi methods
-    #
-
+    #############################################
+    # dbapi methods
+    #############################################
+    # noinspection PyMethodMayBeStatic
     def callproc(self, procname, parameters=None):
         raise errors.NotSupportedError('Cursor.callproc() is not implemented')
 
@@ -47,86 +76,90 @@ class Cursor(object):
         self._closed = True
 
     def execute(self, operation, parameters=None):
+        operation = as_text(operation)
+
         if self.closed():
             raise errors.Error('Cursor is closed')
 
         self.flush_to_query_ready()
 
         if parameters:
-            # # optional requirement
-            import six
-            from psycopg2.extensions import adapt
-
-            if isinstance(parameters, dict):
-                for key in parameters:
-                    param = parameters[key]
-                    # Make sure adapt() behaves properly
-                    if self.is_stringy(param) and six.PY2:
-                        param = param.encode('utf8')
-                    v = adapt(param).getquoted()
-
-                    # Using a regex with word boundary to correctly handle params with similar names
-                    # such as :s and :start
-                    match_str = u':%s\\b' % str(key)
-                    operation = re.sub(match_str, v.decode('utf-8'), operation, flags=re.UNICODE)
-            elif isinstance(parameters, tuple):
-                tlist = []
-                for param in parameters:
-                    if self.is_stringy(param) and six.PY2:
-                        param = param.encode('utf8')
-                    v = adapt(param).getquoted()
-                    tlist.append(v.decode('utf-8'))
-
-                operation = operation % tuple(tlist)
-            else:
-                raise errors.Error("Argument 'parameters' must be dict or tuple")
+            # TODO: quote = True for backward compatibility. see if should be False.
+            operation = self.format_operation_with_parameters(operation, parameters, quote=True)
 
         self.rowcount = -1
 
         self.connection.write(messages.Query(operation))
 
         # read messages until we hit an Error, DataRow or ReadyForQuery
+        self._message = self.connection.read_message()
         while True:
-            message = self.connection.read_message()
-            # save the message because there's no way to undo the read
-            self._message = message
-            if isinstance(message, messages.ErrorResponse):
-                raise errors.QueryError.from_error_response(message, operation)
-            elif isinstance(message, messages.RowDescription):
-                self.description = list(map(lambda fd: Column(fd, self.unicode_error), message.fields))
-            elif isinstance(message, messages.DataRow):
+            if isinstance(self._message, messages.ErrorResponse):
+                raise errors.QueryError.from_error_response(self._message, operation)
+            elif isinstance(self._message, messages.RowDescription):
+                self.description = [Column(fd, self.unicode_error) for fd in self._message.fields]
+            elif isinstance(self._message, messages.DataRow):
                 break
-            elif isinstance(message, messages.ReadyForQuery):
+            elif isinstance(self._message, messages.ReadyForQuery):
+                break
+            elif isinstance(self._message, messages.CommandComplete):
                 break
             else:
-                self.connection.process_message(message)
+                self.connection.process_message(self._message)
 
+            self._message = self.connection.read_message()
 
-    def is_stringy(self, s):
-        try:
-            # python 2 case
-            return isinstance(s, basestring)
-        except NameError:
-            # python 3 case
-            return isinstance(s, str)
+    def executemany(self, operation, seq_of_parameters):
+        operation = as_text(operation)
+
+        if not isinstance(seq_of_parameters, (list, tuple)):
+            raise TypeError("seq_of_parameters should be list/tuple")
+
+        m = self._insert_statement.match(operation)
+        if m:
+            schema = as_text(m.group('schema'))
+            table = as_text(m.group('table'))
+            variables = as_text(m.group('variables'))
+            values = as_text(m.group('values'))
+            if schema is not None:
+                table = "%s.%s" % (schema, table)
+
+            variables = ",".join([variable.strip() for variable in variables.split(",")])
+
+            values = ",".join([value.strip() for value in values.split(",")])
+            seq_of_values = [self.format_operation_with_parameters(values, parameters)
+                             for parameters in seq_of_parameters]
+            data = "\n".join(seq_of_values)
+
+            copy_statement = (
+                "COPY {table} ({variables}) FROM STDIN DELIMITER ',' ENCLOSED BY '\"' "
+                "ENFORCELENGTH ABORT ON ERROR").format(table=table, variables=variables)
+
+            self.copy(copy_statement, data)
+
+        else:
+            raise NotImplementedError(
+                "executemany is implemented for simple INSERT statements only")
 
     def fetchone(self):
-        if isinstance(self._message, messages.DataRow):
-            if self.rowcount == -1:
-                self.rowcount = 1
+        while True:
+            if isinstance(self._message, messages.DataRow):
+                if self.rowcount == -1:
+                    self.rowcount = 1
+                else:
+                    self.rowcount += 1
+                row = self.row_formatter(self._message)
+                # fetch next message
+                self._message = self.connection.read_message()
+                return row
+            elif isinstance(self._message, messages.ReadyForQuery):
+                return None
+            elif isinstance(self._message, messages.CommandComplete):
+                return None
             else:
-                self.rowcount += 1
+                self.connection.process_message(self._message)
 
-            row = self.row_formatter(self._message)
-            # fetch next message
             self._message = self.connection.read_message()
-            return row
-        elif isinstance(self._message, messages.ReadyForQuery):
-            return None
-        elif isinstance(self._message, messages.CommandComplete):
-            return None
-        else:
-            self.connection.process_message(self._message)
 
     def iterate(self):
         row = self.fetchone()
@@ -155,7 +188,7 @@ class Cursor(object):
         self.flush_to_command_complete()
 
         if self._message is None:
-            return None
+            return False
         elif isinstance(self._message, messages.CommandComplete):
             # there might be another set, read next message to find out
             self._message = self.connection.read_message()
@@ -164,29 +197,29 @@ class Cursor(object):
                 self._message = self.connection.read_message()
                 return True
             elif isinstance(self._message, messages.ReadyForQuery):
-                return None
+                return False
             else:
-                raise errors.Error('Unexpected nextset() state after CommandComplete: ' + str(self._message))
+                raise errors.Error(
+                    'Unexpected nextset() state after CommandComplete: {0}'.format(self._message))
         elif isinstance(self._message, messages.ReadyForQuery):
             # no more sets left to be read
-            return None
+            return False
         else:
-            raise errors.Error('Unexpected nextset() state: ' + str(self._message))
+            raise errors.Error('Unexpected nextset() state: {0}'.format(self._message))
 
-
-    def setinputsizes(self):
+    def setinputsizes(self, sizes):
         pass
 
     def setoutputsize(self, size, column=None):
         pass
 
-    #
-    # Non dbApi methods
-    #
+    #############################################
+    # non-dbapi methods
+    #############################################
     def flush_to_query_ready(self):
-        # if the last message isnt empty or ReadyForQuery, read all remaining messages
-        if(self._message is None
-           or isinstance(self._message, messages.ReadyForQuery)):
+        # if the last message isn't empty or ReadyForQuery, read all remaining messages
+        if self._message is None \
+                or isinstance(self._message, messages.ReadyForQuery):
             return
 
         while True:
@@ -197,10 +230,9 @@ class Cursor(object):
                 break
 
     def flush_to_command_complete(self):
-        # if the last message isnt empty or CommandComplete, read messages until it is
-        if(self._message is None
-           or isinstance(self._message, messages.ReadyForQuery)
-           or isinstance(self._message, messages.CommandComplete)):
+        # if the last message isn't empty or CommandComplete, read messages until it is
+        if self._message is None or isinstance(self._message, (messages.ReadyForQuery,
+                                                               messages.CommandComplete)):
             return
 
         while True:
@@ -209,19 +241,30 @@ class Cursor(object):
                 self._message = message
                 break
 
-
-    # example:
-    #
-    # with open("/tmp/file.csv", "rb") as fs:
-    #   cursor.copy("COPY table(field1,field2) FROM STDIN DELIMITER ',' ENCLOSED BY '\"'", fs, buffer_size=65536)
-    #
-
     def copy(self, sql, data, **kwargs):
+        """
+        
+        EXAMPLE:
+        >> with open("/tmp/file.csv", "rb") as fs:
+        >>     cursor.copy("COPY table(field1,field2) FROM STDIN DELIMITER ',' ENCLOSED BY '\"'",
+        >>                 fs, buffer_size=65536)
+
+        """
+        sql = as_text(sql)
 
         if self.closed():
             raise errors.Error('Cursor is closed')
 
         self.flush_to_query_ready()
+
+        if isinstance(data, binary_type):
+            stream = BytesIO(data)
+        elif isinstance(data, text_type):
+            stream = StringIO(data)
+        elif isinstance(data, file_type):
+            stream = data
+        else:
+            raise TypeError("Not valid type of data {0}".format(type(data)))
 
         self.connection.write(messages.Query(sql))
 
@@ -232,29 +275,22 @@ class Cursor(object):
                 raise errors.QueryError.from_error_response(message, sql)
 
             self.connection.process_message(message=message)
+
             if isinstance(message, messages.ReadyForQuery):
                 break
             elif isinstance(message, messages.CopyInResponse):
-
-                #write stuff
-                if not hasattr(data, "read"):
-                    self.connection.write(messages.CopyData(data))
-                else:
-                    # treat data as stream
-                    self.connection.write(messages.CopyStream(data, **kwargs))
-
+                self.connection.write(messages.CopyStream(stream, **kwargs))
                 self.connection.write(messages.CopyDone())
 
         if self.error is not None:
             raise self.error
 
-    #
-    # Internal
-    #
-
     def closed(self):
         return self._closed or self.connection.closed()
 
+    #############################################
+    # internal
+    #############################################
     def row_formatter(self, row_data):
         if self.cursor_type is None:
             return self.format_row_as_array(row_data)
@@ -263,7 +299,7 @@ class Cursor(object):
         elif self.cursor_type in (dict, 'dict'):
             return self.format_row_as_dict(row_data)
         else:
-            raise Exception('Unrecognized cursor_type: %r' % self.cursor_type)
+            raise TypeError('Unrecognized cursor_type: {0}'.format(self.cursor_type))
 
     def format_row_as_dict(self, row_data):
         return OrderedDict(
@@ -274,3 +310,49 @@ class Cursor(object):
     def format_row_as_array(self, row_data):
         return [self.description[idx].convert(value)
                 for idx, value in enumerate(row_data.values)]
+
+    # noinspection PyArgumentList
+    def format_operation_with_parameters(self, operation, parameters, quote=True):
+        operation = as_text(operation)
+
+        if isinstance(parameters, dict):
+            for key, param in six.iteritems(parameters):
+                if not isinstance(key, string_types):
+                    key = str(key)
+                key = as_text(key)
+
+                if isinstance(param, string_types):
+                    param = as_text(param)
+                    if quote:
+                        param = self.format_quote(param)
+                else:
+                    param = str(param)
+                value = as_text(param)
+
+                # Using a regex with word boundary to correctly handle params with similar names
+                # such as :s and :start
+                match_str = u":{0}\\b".format(key)
+                operation = re.sub(match_str, value, operation, flags=re.U)
+
+        elif isinstance(parameters, (tuple, list)):
+            tlist = []
+            for param in parameters:
+                if isinstance(param, string_types):
+                    param = as_text(param)
+                    if quote:
+                        param = self.format_quote(param)
+                else:
+                    param = str(param)
+                value = as_text(param)
+
+                tlist.append(value)
+
+            operation = operation % tuple(tlist)
+        else:
+            raise errors.Error("Argument 'parameters' must be dict or tuple")
+
+        return operation
+
+    def format_quote(self, param):
+        # TODO Make sure adapt() behaves properly
+        return QuotedString(param.encode(UTF_8, self.unicode_error)).getquoted()

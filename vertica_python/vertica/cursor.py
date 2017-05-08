df4c99cda7d28a3069e090e9a2665afcffd8194e
Jeff Kim <jkim@etsy.com>
2017-05-08 14:07:26 -0500

    Maintain compatibility with Python 2.6

--- vertica_python/vertica/cursor.py
+++ vertica_python/vertica/cursor.py
@@ -4,6 +4,11 @@ import re
 import logging
 
 from collections import OrderedDict
+try:
+    from collections import OrderedDict  # python 2.7+ / 3
+except ImportError:
+    from ordereddict import OrderedDict  # python 2.6
+
 from builtins import str
 
 import vertica_python.errors as errors

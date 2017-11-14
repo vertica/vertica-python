75f3064a39850ecf4f386ca0116e5115b4fd7936
Dmitry Volodin <volodin@vm-shiny-dev.rosenergo.com>
2017-11-14 18:47:11 +0300

    Ignore CommandComplete message in cursor message processing

--- vertica_python/vertica/cursor.py
+++ vertica_python/vertica/cursor.py
@@ -121,7 +121,8 @@ class Cursor(object):
             elif isinstance(self._message, messages.ReadyForQuery):
                 break
             elif isinstance(self._message, messages.CommandComplete):
-                break
+                # break
+                pass
             else:
                 self.connection.process_message(self._message)
 

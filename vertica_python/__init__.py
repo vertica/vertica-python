d9a971c51076859eb9a29312a9ec95c9dac95f79
Siting Ren <siting.ren@microfocus.com>
2019-05-06 17:06:11 -0400

    Update module attributes

--- vertica_python/__init__.py
+++ vertica_python/__init__.py
@@ -46,7 +46,7 @@ from .errors import (
     InternalError, NotSupportedError, OperationalError, ProgrammingError)
 
 # Main module for this library.
-__author__ = 'Uber Technologies, Inc'
+__author__ = 'Vertica'
 __copyright__ = 'Copyright (c) 2018-2019 Micro Focus or one of its affiliates.'
 __license__ = 'Apache 2.0'
 
@@ -64,4 +64,9 @@ PROTOCOL_VERSION = 3 << 16 | 5
 
 apilevel = 2.0
 threadsafety = 1  # Threads may share the module, but not connections!
-paramstyle = 'named'  # WHERE name=:name
+
+# Accepted paramstyles are
+#   'qmark' = Question mark style, e.g. '...WHERE name=?'
+#   'named' = Named style, e.g. '...WHERE name=:name'
+#   'format' = ANSI C printf format codes, e.g. '...WHERE name=%s'
+paramstyle = 'named'

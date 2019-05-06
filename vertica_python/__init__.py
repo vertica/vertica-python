b004c4449ce5a92d21dfe9d0215b8ad594376546
Siting Ren <ren_siting@hotmail.com>
2019-05-06 17:26:43 -0400

    Update Copyright year (#285)

--- vertica_python/__init__.py
+++ vertica_python/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Micro Focus or one of its affiliates.
+# Copyright (c) 2018-2019 Micro Focus or one of its affiliates.
 # Copyright (c) 2018 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -46,8 +46,8 @@ from .errors import (
     InternalError, NotSupportedError, OperationalError, ProgrammingError)
 
 # Main module for this library.
-__author__ = 'Uber Technologies, Inc'
-__copyright__ = 'Copyright (c) 2018 Micro Focus or one of its affiliates.'
+__author__ = 'Vertica'
+__copyright__ = 'Copyright (c) 2018-2019 Micro Focus or one of its affiliates.'
 __license__ = 'Apache 2.0'
 
 __all__ = ['Connection', 'PROTOCOL_VERSION', 'version_info', 'apilevel', 'threadsafety',
@@ -64,4 +64,9 @@ PROTOCOL_VERSION = 3 << 16 | 5
 
 apilevel = 2.0
 threadsafety = 1  # Threads may share the module, but not connections!
-paramstyle = 'named'  # WHERE name=:name
+
+# Accepted paramstyles are
+#   'qmark' = Question mark style, e.g. '...WHERE name=?'
+#   'named' = Named style, e.g. '...WHERE name=:name'
+#   'format' = ANSI C printf format codes, e.g. '...WHERE name=%s'
+paramstyle = 'named'

f9da16d705007fb6b86c01dce8fa1514d635de22
Siting Ren <sitingren@hotmail.com>
2019-10-17 13:53:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Database",
         "Topic :: Database :: Database Engines/Servers",
         "Operating System :: OS Independent"

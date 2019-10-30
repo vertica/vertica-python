38294db67ff4937f2d10b15e426108464bee572d
Siting Ren <siting.ren@microfocus.com>
2019-10-30 12:42:18 -0400

    Update copyright statements

--- vertica_python/tests/unit_tests/test_notice.py
+++ vertica_python/tests/unit_tests/test_notice.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2018-2019 Micro Focus or one of its affiliates.
-# Copyright (c) 2018 Uber Technologies, Inc.
+# Copyright (c) 2019 Micro Focus or one of its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,26 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright (c) 2013-2017 Uber Technologies, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 from __future__ import print_function, division, absolute_import
 
 import mock
@@ -40,6 +19,7 @@ import mock
 from .base import VerticaPythonUnitTestCase
 from ...vertica.messages import NoticeResponse
 from ...errors import QueryError
+
 
 class NoticeTestCase(VerticaPythonUnitTestCase):
     SAMPLE_DATA = {b'S': 'FATAL',

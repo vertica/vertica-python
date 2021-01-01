3d202a1785911ff8348190cce5c30c452b3dfeff
Siting Ren <sitingren@hotmail.com>
2021-01-01 09:53:46 +0800

    Update copyright year to 2021 (#404)

--- vertica_python/tests/unit_tests/test_logging.py
+++ vertica_python/tests/unit_tests/test_logging.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020 Micro Focus or one of its affiliates.
+# Copyright (c) 2019-2021 Micro Focus or one of its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

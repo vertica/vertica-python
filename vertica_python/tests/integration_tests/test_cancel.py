c2e286b7d469107de6f99594bef0b9dc2b665662
Siting Ren <siting.ren@microfocus.com>
2021-08-24 11:06:21 -0400

    Add cancel() test

--- vertica_python/tests/integration_tests/test_cancel.py
+++ vertica_python/tests/integration_tests/test_cancel.py
@@ -55,5 +55,39 @@ class CancelTestCase(VerticaPythonIntegrationTestCase):
                 cur.execute(long_running_query)
             p1.join()
 
+    def test_connection_cancel_returned_query(self):
+        with self._connect() as conn:
+            cur = conn.cursor()
+            cur.execute("DROP TABLE IF EXISTS vptest")
+            try:
+                # Creating and loading table
+                cur.execute("CREATE TABLE vptest(id INTEGER, time TIMESTAMP)")
+                cur.execute("INSERT INTO vptest"
+                        " SELECT row_number() OVER(), slice_time"
+                        "    FROM("
+                        "         SELECT slice_time FROM("
+                        "         SELECT '2021-01-01'::timestamp s UNION ALL SELECT '2022-01-01'::timestamp s"
+                        "          ) sq TIMESERIES slice_time AS '1 second' OVER(ORDER BY s)"
+                        "    ) sq2")
+
+                # This query returns over 30,000,000 rows. We cancel the command after
+                # reading 100 of them, and then continue reading results. This quickly
+                # results in an exception being thrown due to the cancel having taken effect.
+                cur.execute("SELECT id, time FROM vptest")
+
+                nCount = 0
+                with self.assertRaises(errors.QueryCanceled):
+                    while cur.fetchone():
+                        nCount += 1
+                        if nCount == 100:
+                            conn.cancel()
+
+                # The number of rows read after the cancel message is sent to the server can vary.
+                # 100,000 seems to leave a safe margin while still falling well short of
+                # the 30,000,000+ rows we'd have read if the cancel didn't work.
+                self.assertTrue(100 <= nCount < 100000)
+            finally:
+                cur.execute("DROP TABLE IF EXISTS vptest")
+
 
 exec(CancelTestCase.createPrepStmtClass())

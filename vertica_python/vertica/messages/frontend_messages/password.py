72080819993fb9c0f51860cacac69cdf1cdd07f6
Siting Ren <siting.ren@microfocus.com>
2022-01-05 08:04:12 -0500

    update copyright year to 2022

--- vertica_python/vertica/messages/frontend_messages/password.py
+++ vertica_python/vertica/messages/frontend_messages/password.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2021 Micro Focus or one of its affiliates.
+# Copyright (c) 2018-2022 Micro Focus or one of its affiliates.
 # Copyright (c) 2018 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");

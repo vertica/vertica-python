96037d331af615a33488f30f0251d1e3fe301490
AlonMenczer <alon.menczer@gmail.com>
2022-04-02 12:36:45 +0300

    remove previous read_bytes version

--- vertica_python/vertica/connection.py
+++ vertica_python/vertica/connection.py
@@ -697,26 +697,7 @@ class Connection(object):
             self._logger.error(msg)
             raise errors.MessageError(msg)
 
-    # TODO: left for testing
     def read_bytes(self, n):
-        if n == 1:
-            result = self._socket().recv(1)
-            if not result:
-                raise errors.ConnectionError("Connection closed by Vertica")
-            return result
-        else:
-            buf = bytearray(n)
-            view = memoryview(buf)
-            to_read = n
-            while to_read > 0:
-                received = self._socket().recv_into(view, to_read)
-                if received == 0:
-                    raise errors.ConnectionError("Connection closed by Vertica")
-                view = view[received:]
-                to_read -= received
-            return buf
-
-    def read_bytes_using_file(self, n):
         if n == 1:
             result = self.socket_as_file.read(1)
             if not result:
@@ -733,8 +714,6 @@ class Connection(object):
                 if received == 0:
                     raise errors.ConnectionError("Connection closed by Vertica")
             return buf
-
-    read_bytes = read_bytes_using_file
 
     def send_GSS_response_and_receive_challenge(self, response):       
         # Send the GSS response data to the vertica server

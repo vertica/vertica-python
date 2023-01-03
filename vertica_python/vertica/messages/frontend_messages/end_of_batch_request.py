ab5a3b47baa742aa9983a6bbc894fd7a97c6f49d
Siting Ren <siting.ren@microfocus.com>
2023-01-03 22:11:41 +0800

    Update copyright year to 2023 (#480)

--- vertica_python/vertica/messages/frontend_messages/end_of_batch_request.py
+++ vertica_python/vertica/messages/frontend_messages/end_of_batch_request.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2022 Micro Focus or one of its affiliates.
+# Copyright (c) 2020-2023 Micro Focus or one of its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

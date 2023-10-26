f1f38900f2bd649793f600b77e88fa406057b393
Siting Ren <sitingren@hotmail.com>
2023-10-26 19:48:14 +0800

    updates

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Database",
         "Topic :: Database :: Database Engines/Servers",
         "Topic :: Database :: Front-Ends",

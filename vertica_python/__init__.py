a536e2b032a07c15bebd8531fb4b317c744a3426
Siting Ren <sren2@opentext.com>
2023-12-18 21:01:35 -0500

    Bump version to 1.3.8, fix packaging

--- vertica_python/__init__.py
+++ vertica_python/__init__.py
@@ -56,7 +56,7 @@ __all__ = ['Connection', 'PROTOCOL_VERSION', 'version_info', 'apilevel', 'thread
            'OperationalError', 'ProgrammingError']
 
 # The version number of this library.
-version_info = (1, 3, 7)
+version_info = (1, 3, 8)
 __version__ = '.'.join(map(str, version_info))
 
 # The protocol version (3.15) implemented in this library.

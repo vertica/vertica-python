8c4f7e1dbdb2763efafb0c9032a89e704207e69e
sitingren <sren2@opentext.com>
2024-06-18 04:12:10 +0000

    add tlsmode.py

---
+++ vertica_python/vertica/tlsmode.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2024 Open Text.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import print_function, division, absolute_import, annotations
+
+from enum import Enum
+
+class TLSMode(Enum):
+    DISABLE = 'disable'
+    PREFER = 'prefer'
+    REQUIRE = 'require'
+    VERIFY_CA = 'verify-ca'
+    VERIFY_FULL = 'verify-full'
+
+    def requests_encryption(self) -> bool:
+        return self != TLSMode.DISABLE
+
+    def requires_encryption(self) -> bool:
+        return self not in (TLSMode.DISABLE, TLSMode.PREFER)
+
